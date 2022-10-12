461f661829966d51a466b15963766dc1d12da242
Harish <harishtutor12@gmail.com>
2022-10-12 22:56:31 +0530

    Removed json server

--- src/delete_request.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-axios.delete('http://localhost:3000/employees/6/')
-    .then(resp => {
-        console.log(resp.data)
-    }).catch(error => {
-        console.log(error);
-});
